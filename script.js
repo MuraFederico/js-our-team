74e0fd36c30bc286360d44402110be5a0fab35ef
MuraFederico <federicomura98@gmail.com>
2022-03-07 13:42:27 +0100

    inizio esercizio

---
+++ script.js
@@ -0,0 +1,42 @@
+const members = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        img: 'img/wayne-barnett-founder-ceo.jpg'
+    },
+
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        img: 'img/angela-caroll-chief-editor.jpg'
+    },
+
+    {
+        name: 'Walter Gordon',
+        role: 'Office manager',
+        img: 'img/walter-gordon-office-manager.jpg'
+    },
+
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        img: 'img/angela-lopez-social-media-manager.jpg'
+    },
+
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        img: 'img/scott-estrada-developer.jpg'
+    },
+
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        img: 'img/barbara-ramos-graphic-designer.jpg'
+    },
+
+]
+
+
+const cardsContainer = document.querySelector('.team-container');
+
